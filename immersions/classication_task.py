add maestro dataset, fix error in step-wise loss calculation

immersions/classication_task.py
@@ -7,19 +7,17 @@ from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 
 import pytorch_lightning as pl
-from immersions.audio_dataset import AudioTestingDataset
+from immersions.audio_dataset import AudioTestingDataset, MaestroTestingDataset
 
 
 class ClassificationTaskModel(pl.LightningModule):
     def __init__(self, cpc_system, task_dataset_path, evaluation_ratio=0.2):
         super(ClassificationTaskModel, self).__init__()
         # not the best model...
-        self.cpc_system = cpc_system
+        self.__cpc_system = cpc_system,
+        self.task_dataset_path = task_dataset_path
+        self.audio_dataset = self._load_dataset()
 
-        self.audio_dataset = AudioTestingDataset(location=task_dataset_path,
-                                                 item_length=cpc_system.item_length,
-                                                 sampling_rate=cpc_system.hparams.sampling_rate,
-                                                 unique_length=44100*4)
         self.num_items = len(self.audio_dataset)
         self.classifier_model = torch.nn.Sequential(
             torch.nn.Linear(in_features=self.cpc_system.hparams.ar_channels[-1], out_features=32),
@@ -43,6 +41,12 @@ class ClassificationTaskModel(pl.LightningModule):
         self.validation_indices = index_list[:int(self.num_items*evaluation_ratio)]
         self.training_indices = index_list[int(self.num_items*evaluation_ratio):]
 
+    def _load_dataset(self):
+        return AudioTestingDataset(location=self.task_dataset_path,
+                                   item_length=self.cpc_system.item_length,
+                                   sampling_rate=self.cpc_system.hparams.sampling_rate,
+                                   unique_length=44100*4)
+
     def calculate_data(self):
         # calculate data for test task
         batch_size = self.cpc_system.hparams.batch_size
@@ -62,6 +66,8 @@ class ClassificationTaskModel(pl.LightningModule):
             # if self.preprocessing is not None:
             #     batch = self.preprocessing(batch)
             predictions, targets, z, c = self.cpc_system(batch)
+            c = c.view(batch.shape[0], -1, c.shape[1])
+            c = c[:, 0, :]
             # z = self.model.encoder(batch.unsqueeze(1))
             # c = self.model.autoregressive_model(z)
             task_data[step * batch_size:step*batch_size + c.shape[0], :] = c.detach().cpu()
@@ -69,9 +75,9 @@ class ClassificationTaskModel(pl.LightningModule):
 
         del t_dataloader
 
-        task_data = task_data.detach()
-        task_labels = task_labels.detach()
-        self.encoding_dataset = torch.utils.data.TensorDataset(task_data, task_labels)
+        self.task_data = task_data.detach()
+        self.task_labels = task_labels.detach()
+        self.encoding_dataset = torch.utils.data.TensorDataset(self.task_data, self.task_labels)
 
     def forward(self, x):
         return self.classifier_model(x)
@@ -108,3 +114,21 @@ class ClassificationTaskModel(pl.LightningModule):
     def val_dataloader(self):
         dataset = torch.utils.data.Subset(self.encoding_dataset, self.validation_indices)
         return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size)
+
+    # def train(self, mode=True):
+    #     self.classifier_model.train(mode)
+    #
+    @property
+    def cpc_system(self):
+        return self.__cpc_system[0]
+
+
+class MaestroClassificationTaskModel(ClassificationTaskModel):
+    def _load_dataset(self):
+        return MaestroTestingDataset(location=self.task_dataset_path,
+                                     item_length=self.cpc_system.item_length,
+                                     sampling_rate=self.cpc_system.hparams.sampling_rate,
+                                     unique_length=44100*4,
+                                     mode='validation',
+                                     max_file_count=10,
+                                     shuffle_with_seed=123)

immersions/cpc_system.py
@@ -140,18 +140,14 @@ class ContrastivePredictiveSystem(LightningModule):
 
         # scores: data_batch, data_step, target_batch, target_step
         if self.hparams.score_over_all_timesteps:
-            noise_scoring = torch.logsumexp(scores.view(-1, batch_size, self.prediction_steps),
-                                            dim=0)  # target_batch, target_step
-            score_sum = torch.sum(scores.view(-1, batch_size, self.prediction_steps),
-                                  dim=0)
+            n_scores = scores.view(-1, batch_size, self.prediction_steps)  # data_batch*data_step, target_batch. target_step
+            noise_scoring = torch.logsumexp(n_scores, dim=0)  # target_batch, target_step
             valid_scores = torch.diagonal(scores, dim1=0, dim2=2)  # data_step, target_step, batch
             valid_scores = torch.diagonal(valid_scores, dim1=0, dim2=1)  # batch, step
         else:
-            scores = torch.diagonal(scores, dim1=1, dim2=3).permute(
-                [0, 2, 1]).contiguous()  # data_batch, step, target_batch
-            noise_scoring = torch.logsumexp(scores.view(-1, batch_size, self.prediction_steps),
-                                            dim=0)  # target_batch, target_step
-            valid_scores = torch.diagonal(scores, dim1=0, dim2=2).permute([1, 0])  # batch, step
+            scores = torch.diagonal(scores, dim1=1, dim2=3)  # data_batch, target_batch, step
+            noise_scoring = torch.logsumexp(scores, dim=0)  # target_batch, target_step
+            valid_scores = torch.diagonal(scores, dim1=0, dim2=1).permute([1, 0])  # batch, step
 
         prediction_losses = -torch.mean(valid_scores - noise_scoring, dim=1)
         loss = torch.mean(prediction_losses)
@@ -274,11 +270,25 @@ class ContrastivePredictiveSystem(LightningModule):
         val_loss_mean /= len(outputs)
         val_acc_mean /= len(outputs)
 
+        test_task_dict = {}
         if self.test_task_model is not None:
-            pass
+            self.test_task_model.calculate_data()
+
+            if not self.experiment.debug:
+                self.experiment.add_embedding(self.test_task_model.task_data, metadata=self.test_task_model.task_labels,
+                                              global_step=self.global_step)
+            if torch.cuda.is_available():
+                trainer = pl.Trainer(max_nb_epochs=20,
+                                     gpus=[0])
+            else:
+                trainer = pl.Trainer(max_nb_epochs=20)
+            trainer.fit(self.test_task_model)
+
+            test_task_dict['val_task_acc'] = trainer.tng_tqdm_dic['avg_val_accuracy']
+            test_task_dict['val_task_loss'] = trainer.tng_tqdm_dic['avg_val_loss']
 
         tqdm_dic = {'val_loss': val_loss_mean, 'val_acc': val_acc_mean}
-        return tqdm_dic
+        return {**tqdm_dic, **test_task_dict}
 
     # ---------------------
     # TRAINING SETUP

immersions/model/predictive_coding.py
@@ -40,19 +40,34 @@ class PredictiveCodingModel(nn.Module):
         x = self.input_activation_writer(x)
 
         z = self.encoder(x)  # batch x enc_dim x steps
+        n, e, _ = z.shape
         if self.detach_targets:
-            targets = z[:, :, -self.prediction_steps:].detach()  # batch, enc_size, step
+            targets = z[:, :, self.autoregressive_model.receptive_field:].detach()  # batch, enc_size, step
         else:
-            targets = z[:, :, -self.prediction_steps:]  # batch, enc_size, step
-        z = z[:, :, -(self.visible_steps+self.prediction_steps):-self.prediction_steps]
+            targets = z[:, :, self.autoregressive_model.receptive_field:]  # batch, enc_size, step
+
+        num_predictions = (targets.shape[2] - self.prediction_steps) // self.autoregressive_model.downsampling_factor + 1
+        prediction_indices = torch.arange(self.prediction_steps).to(x.device)
+        prediction_offsets = torch.arange(num_predictions).to(x.device) * self.autoregressive_model.downsampling_factor
+        prediction_indices = prediction_offsets[:, None] + prediction_indices[None, :]
+        targets = targets[:, :, prediction_indices]  # batch, enc_size, c_step, step
+        targets = targets.permute(0, 2, 1, 3).contiguous().view(n*num_predictions, e, self.prediction_steps)  # batchxc_step, enc_size, step
+
+
+
+        z = z[:, :, :-self.prediction_steps]
 
         z = self.z_activation_writer(z)
 
-        c = self.autoregressive_model(z)
-        if len(c.shape) == 3:
-            c = c[:, :, 0]
+        c = self.autoregressive_model(z)  # batch, enc_size, c_step
+        c = c[:, :, :num_predictions]
+        # if len(c.shape) == 3:
+        #     c = c[:, :, 0]
 
         c = self.c_activation_writer(c)
+
+        c = c.permute(0, 2, 1).contiguous().view(n*num_predictions, -1)  # batch, c_step, c_enc_size
+
 
         predicted_z = self.prediction_model(c)  # batch, step*enc_size
         predicted_z = predicted_z.view(-1, self.prediction_steps, self.enc_size)  # batch, step, enc_size